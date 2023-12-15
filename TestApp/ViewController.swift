b609e9b298792d2f1e058780cbdc1d0fd78a6eed
Adavan Ayyappan <>
2023-12-16 00:40:56 +0530

    setup project and folder structure

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Adavan Ayyappan on 15/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
