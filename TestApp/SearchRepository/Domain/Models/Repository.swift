152da0df3ddaf85fcf4d6b9c5ce82d19d0346f75
Adavan Ayyappan <>
2023-12-16 00:42:01 +0530

    Added respository data model classes

---
+++ TestApp/SearchRepository/Domain/Models/Repository.swift
@@ -0,0 +1,13 @@
+//
+//  Repository.swift
+//  TestApp
+//
+//  Created by Adavan Ayyappan on 15/12/23.
+//
+
+struct Repository: Codable {
+    let name: String?
+    let fullName: String?
+    let language: String?
+    let createdAt: String?
+}
---
+++ TestApp/SearchRepository/Domain/Models/RepositoryList.swift
@@ -0,0 +1,10 @@
+//
+//  RepositoryList.swift
+//  TestApp
+//
+//  Created by Adavan Ayyappan on 15/12/23.
+//
+
+struct RepositoryList: Codable {
+    let items: [Repository]
+}
